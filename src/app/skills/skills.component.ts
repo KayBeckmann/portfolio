34a8526846961f4b6db04edab8a5af3e4a70d29a
Kay <info@beckmann-md.de>
2023-06-29 17:16:43 +0200

    links for menu. Skill icons.

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-skills',
-  templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.scss']
+  selector: "app-skills",
+  templateUrl: "./skills.component.html",
+  styleUrls: ["./skills.component.scss"]
 })
 export class SkillsComponent {
-skills=["angular", "api", "automation", "css", "firebase", "git", "html", "javascript", "scrum", "typescript", "wordpress"]
+  skills = [
+    ["angular", "api", "automation", "css"],
+    ["firebase", "git", "html"],
+    ["javascript", "scrum"],
+    ["typescript"]
+  ]; //, "wordpress"]
 }
