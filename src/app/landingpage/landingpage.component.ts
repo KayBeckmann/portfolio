51727cecd9e8c5c939143aac70b669fbca608080
Kay Beckmann <info@beckmann-md.de>
2023-07-19 22:01:40 +0200

    Links angepasst

--- src/app/landingpage/landingpage.component.ts
+++ src/app/landingpage/landingpage.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-landingpage',
-  templateUrl: './landingpage.component.html',
-  styleUrls: ['./landingpage.component.scss']
+  selector: "app-landingpage",
+  templateUrl: "./landingpage.component.html",
+  styleUrls: ["./landingpage.component.scss"]
 })
 export class LandingpageComponent {
-
+  gotoBottom() {
+    window.scroll({
+      top: document.body.scrollHeight,
+      left: 0,
+      behavior: "smooth"
+    });
+  }
 }
