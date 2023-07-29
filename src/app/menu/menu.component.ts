30b0e6df0b55b1daff5181b3899f9b36253e591c
Kay <info@beckmann-md.de>
2023-07-29 11:32:51 +0200

    finished mobile menu

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-menu',
-  templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  selector: "app-menu",
+  templateUrl: "./menu.component.html",
+  styleUrls: ["./menu.component.scss"]
 })
 export class MenuComponent {
+  element!: HTMLElement;
 
+  constructor() {}
+
+  /**
+   * Go to Section with HTML-ID
+   * @param id (String)
+   */
+  goTo(id: string) {
+    this.element = document.getElementById(id) as HTMLElement;
+    this.element.scrollIntoView();
+
+    this.toggleMenu();
+  }
+
+  /**
+   * Toggle mobilemenu view
+   */
+  toggleMenu() {
+    this.element = document.getElementById("mobilemenu") as HTMLElement;
+    if (this.element.classList.contains("d-none")) {
+      this.element.classList.remove("d-none");
+    } else {
+      this.element.classList.add("d-none");
+    }
+    console.log("TOGGLE");
+  }
 }
