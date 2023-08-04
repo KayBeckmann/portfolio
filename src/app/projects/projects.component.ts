ff3f8c4925f5c0f17855eda8574dfc91c0f6aa2f
Kay <info@beckmann-md.de>
2023-08-04 16:55:45 +0200

    Github links added. change project.

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -10,25 +10,31 @@ export class ProjectsComponent {
     {
       title: "El Pollo Loco",
       description: "Jump and run game.",
-      github: "https://#",
+      github: "https://github.com/KayBeckmann/el_pollo_loco",
       live: "https://#"
     },
     {
       title: "Join",
       description: "Canban Board for your projects.",
-      github: "https://#",
+      github: "https://github.com/KayBeckmann/join",
       live: "https://#"
     },
-    {
+    /*{
       title: "CRM",
       description: "Project don't finished jet",
       github: "https://#",
       live: "https://#"
-    },
+    },*/
     {
       title: "Ring of fire",
       description: "Online drinking game with your friends.",
-      github: "https://#",
+      github: "https://github.com/KayBeckmann/ringoffire",
+      live: "https://#"
+    },
+    {
+      title: "Portfolio",
+      description: "This page.",
+      github: "https://github.com/KayBeckmann/portfolio",
       live: "https://#"
     }
   ];
