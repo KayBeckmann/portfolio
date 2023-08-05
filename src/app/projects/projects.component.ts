25ebad7be09a805a6bcaa29adf0ebe8c950a2a81
Kay <info@beckmann-md.de>
2023-08-05 21:46:03 +0200

    added project pictures

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -10,30 +10,35 @@ export class ProjectsComponent {
     {
       title: "El Pollo Loco",
       description: "Jump and run game.",
+      img: "",
       github: "https://github.com/KayBeckmann/el_pollo_loco",
       live: "https://#"
     },
     {
       title: "Join",
       description: "Canban Board for your projects.",
+      img: "",
       github: "https://github.com/KayBeckmann/join",
       live: "https://#"
     },
     /*{
       title: "CRM",
       description: "Project don't finished jet",
+      img:"",
       github: "https://#",
       live: "https://#"
     },*/
     {
       title: "Ring of fire",
       description: "Online drinking game with your friends.",
+      img: "",
       github: "https://github.com/KayBeckmann/ringoffire",
       live: "https://#"
     },
     {
       title: "Portfolio",
       description: "This page.",
+      img: "",
       github: "https://github.com/KayBeckmann/portfolio",
       live: "https://#"
     }
